8b5d241bfd08b9d77171477c4c71533f6490985e
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-21 16:26:58 -0700

    Add colors and fonts

---
+++ src/constants/data.ts
@@ -0,0 +1,13 @@
+const info = {}
+const about = {}
+const experience = {}
+const skills = {}
+const projects = {}
+
+export default {
+  info,
+  about,
+  experience,
+  skills,
+  projects,
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
+        primary: "#222B2F",
+        secondary: "#38464D",
+        yellow: "#FFD500",
+        black: "#1A2023",
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        Pacifico: ["Pacifico", "cursive"],
+        Poppins: ["Poppins", "sans-serif"],
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
       },
       screens: {
-        xs: "450px",
-      },
-      backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        xs: "375px",
       },
     },
   },
