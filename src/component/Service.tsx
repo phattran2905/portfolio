ab4bc6935b910e8245554bb9996f163c6e9a2408
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-23 15:53:37 -0700

    Add lifting effect when hovering services

--- src/component/Service.tsx
+++ src/component/Service.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function Service({ title, description, icon }: Props) {
   return (
-    <div className="flex flex-row items-stretch justify-between gap-x-8 rounded-lg border-2 border-extra-light-gray p-6 shadow-md">
+    <div className="flex flex-row items-stretch justify-between gap-x-8 rounded-lg border-2 border-extra-light-gray p-6 shadow-md hover:-translate-y-2 duration-150 ease-in-out hover:shadow-xl">
       <div>{icon}</div>
       <div>
         <h5 className="font-semibold capitalize">{title}</h5>
