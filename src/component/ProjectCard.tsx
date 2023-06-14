50f7e41a08cbc3e70c28f9990e5023cf2cf48a3a
ducphat@Mbp2019 <phat.tranducvn@gmail.com>
2023-06-13 22:01:50 -0700

    Add label for tech stack

--- src/component/ProjectCard.tsx
+++ src/component/ProjectCard.tsx
@@ -30,48 +30,56 @@ function ProjectCard({
         />
       </div>
       <div className="flex h-full w-full flex-col gap-y-4 border-t-2 border-t-primary px-5 py-4">
-        <p className={`my-4 text-[16px] font-normal text-tertiary`}>
+        <p
+          className={`my-4 min-h-[70px] text-[16px] font-normal text-tertiary`}
+        >
           {description}
         </p>
 
-        <div className="mt-auto flex flex-col gap-y-4">
-          <div className="flex flex-col gap-y-1">
-            <span className="block basis-1/3 text-[14px] font-semibold">
-              Tech Stack
-            </span>
-            <ul className="flex basis-2/3 list-none flex-row flex-wrap gap-2">
-              {techStackImages.map((tech, index) => (
-                <li
-                  key={index}
-                  className="h-[32px] w-[32px] rounded-lg border-[1px] border-gray p-[1px] shadow-sm shadow-gray"
-                >
+        <div className="flex flex-col gap-y-1">
+          <span className="my-2 block text-[14px] font-semibold">
+            Tech Stack
+          </span>
+          <ul className="flex w-full list-none flex-row flex-wrap gap-2">
+            {techStackImages.map((tech, index) => (
+              <li
+                key={index}
+                className="flex flex-row items-center rounded-lg border-[1px] border-gray p-1 p-[1px] shadow-sm shadow-gray"
+              >
+                <div className="h-[24px] w-[24px] ">
                   <img
                     src={tech.img}
                     alt={tech.name}
                     className="h-[100%] w-[100%] object-fill"
                   />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="mt-3 flex w-full flex-row items-center justify-center gap-x-4">
-            <a
-              href={demoUrl}
-              target="_blank"
-              className="flex basis-1/2 flex-row items-center justify-center rounded-lg bg-primary px-4 py-2 font-semibold text-white transition duration-200 hover:bg-black"
-            >
-              <FiExternalLink size={16} className="mr-2" />
-              Demo
-            </a>
-            <a
-              href={githubUrl}
-              target="_blank"
-              className="flex basis-1/2 flex-row items-center justify-center rounded-lg bg-tertiary px-4 py-2 font-semibold text-white transition duration-200 hover:bg-black"
-            >
-              <BsGithub size={16} className="mr-2" />
-              GitHub
-            </a>
-          </div>
+                </div>
+                <span className="ml-1 block text-[11px] capitalize">
+                  {tech.name}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-4 px-5 py-4">
+        <div className="flex w-full flex-row items-center justify-center gap-x-4">
+          <a
+            href={demoUrl}
+            target="_blank"
+            className="flex basis-1/2 flex-row items-center justify-center rounded-lg bg-primary px-4 py-2 font-semibold text-white transition duration-200 hover:bg-black"
+          >
+            <FiExternalLink size={16} className="mr-2" />
+            Demo
+          </a>
+          <a
+            href={githubUrl}
+            target="_blank"
+            className="flex basis-1/2 flex-row items-center justify-center rounded-lg bg-tertiary px-4 py-2 font-semibold text-white transition duration-200 hover:bg-black"
+          >
+            <BsGithub size={16} className="mr-2" />
+            GitHub
+          </a>
         </div>
       </div>
     </div>
